f9bcc13229c3a4dbd28910c23811f56ffde0f864
Brad Sickles <brad.sickles@gmail.com>
2025-11-03 10:48:32 -0500

    Adjusted scaling policies to use "Sum" instead of "Average".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
-# 0.1.1 (Dec 28, 2023)
+# 0.1.1 (Nov 03, 2025)
+* Adjusted scaling policies to use "Sum" instead of "Average". 
+
+# 0.1.0 (Dec 28, 2023)
 * Initial draft
--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "metric_alarms" {
       metric_name         = "RequestCountPerTarget"
       namespace           = "AWS/ApplicationELB"
       period              = var.scale_up_period
-      statistic           = "Average"
+      statistic           = "Sum"
       threshold           = var.scale_up_threshold
       alarm_description   = "Triggers a scale up of containers when the TargetGroup has a high number of requests per target"
       actions             = jsonencode([aws_appautoscaling_policy.scale_up.arn])
@@ -23,7 +23,7 @@ output "metric_alarms" {
       metric_name         = "RequestCountPerTarget"
       namespace           = "AWS/ApplicationELB"
       period              = var.scale_down_period
-      statistic           = "Average"
+      statistic           = "Sum"
       threshold           = var.scale_down_threshold
       alarm_description   = "Triggers a scale down of containers when the TargetGroup has a low number of requests per target"
       actions             = jsonencode([aws_appautoscaling_policy.scale_down.arn])
